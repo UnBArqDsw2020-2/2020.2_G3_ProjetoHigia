a0e45660bfdd63cb8f03e1947b3af96496fb3627
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-12 20:11:20 -0300

    [#39] Criando sistema de rotas.
    Co-authored-by: Arthur Paiva <arthur001123@gmail.com>

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/context/auth';
+import Routes from './src/routes';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
