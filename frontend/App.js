5559d3206ab7bc9f4d24768144e535dc54c8de0c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-13 15:31:08 -0300

    [#39] Configurando firebase.
    Co-authored-by: Arthur Paiva <arthur001123@gmail.com>

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/auth';
+import firebase from 'firebase';
+import apiKeys from './src/utils/firebaseKeys.js';
 import Routes from './src/routes';
 
 const App = () => {
+  if (!firebase.apps.length) {
+    console.log('Connected with Firebase');
+    firebase.initializeApp(apiKeys.firebaseConfig);
+  }
   return (
     <NavigationContainer>
       <AuthProvider>
