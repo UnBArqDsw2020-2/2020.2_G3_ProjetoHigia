e1306e8ec8604283ebbab5efda83af7d9ffbe19c
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-18 18:41:00 -0300

    [#46] Adicionando e configurando fonte personalizada.
    Co-Authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>
    Co-Authored-by: Aline <aline.helena.lermen@gmail.com>

--- frontend/src/routes/app.routes.js
+++ frontend/src/routes/app.routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from '../pages/Home';
+import MedicalReport from '../pages/MedicalReport';
 
 const AppStack = createStackNavigator();
 
 const AppRoutes = () => (
   <AppStack.Navigator>
-    <AppStack.Screen name="Home" component={Home} />
+    <AppStack.Screen name="MedicalReport" component={MedicalReport} />
   </AppStack.Navigator>
 );
 
