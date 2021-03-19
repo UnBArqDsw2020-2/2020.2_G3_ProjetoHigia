f38e79c398b46dcfbf9fbfbcc3d85e8bf5c948e2
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-19 12:08:39 -0300

    [#46] Criando dados mockados para teste.
    Co-Authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>
    Co-Authored-by: Aline <aline.helena.lermen@gmail.com>

---
+++ frontend/src/utils/mocks.js
@@ -0,0 +1,39 @@
+export const user = {
+  name: 'Lorem Ipsum',
+  age: 35,
+  height: 170,
+  weight: 70,
+  bloodGroup: 'AB+',
+  medicament: [
+    {
+      id: 1,
+      name: 'Rivotril  -  50mg',
+    },
+    {
+      id: 2,
+      name: 'Roacutan - 100mg',
+    },
+  ],
+  allergy: [
+    {
+      id: 1,
+      name: 'Polén',
+    },
+    {
+      id: 2,
+      name: 'Amendoim',
+    },
+  ],
+  emergencyContacts: [
+    {
+      id: 1,
+      name: 'Lorem Ipsum Jr.',
+      number: '(61) 99999-9999  ',
+    },
+    {
+      id: 2,
+      name: 'Lorem Ipsum Jr.',
+      number: '(61) 99999-9999  ',
+    },
+  ],
+};
