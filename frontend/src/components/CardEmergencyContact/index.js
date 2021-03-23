67f054f4eeaf1f0a3dd0cac84025ab6f302bb52b
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-23 18:26:28 -0300

    [#46] Criando card para contatos de emergência como componente.

---
+++ frontend/src/components/CardEmergencyContact/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import { FontAwesome as Icon } from '@expo/vector-icons';
+import styles from './styles';
+
+const CardEmergencyContact = ({ name, number, edit, onPress }) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <TextInput style={styles.name} value={name ?? 'Nome'} editable={edit} />
+
+        <TextInput
+          style={styles.number}
+          value={number ?? 'Número'}
+          editable={edit}
+        />
+      </View>
+
+      {edit ? (
+        <TouchableOpacity onPress={onPress}>
+          <Icon name="trash" size={20} color="#86172D" />
+        </TouchableOpacity>
+      ) : null}
+    </View>
+  );
+};
+
+export default CardEmergencyContact;
