91918d14b9952477c3805ba8fdb4bf2450042423
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-23 21:49:49 -0300

    [#47] Atualizando página de Buscar Paciente e corrigindo indentação.

--- frontend/src/pages/SearchPacient/index.js
+++ frontend/src/pages/SearchPacient/index.js
@@ -1,38 +1,30 @@
-import React from 'react'
-import { View, Text, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
-import styles from './styles'
-import { MaterialIcons as Icon } from '@expo/vector-icons'
+import React from 'react';
+import { View, Text, ImageBackground, TextInput } from 'react-native';
+import styles from './styles';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
+import Button from '../../components/Button';
 
 const SearchPacient = () => {
-    return (
-        <ImageBackground 
-          style={styles.container}
-          source={require('../../../assets/logo.jpg')} 
-          imageStyle={{width: '100%', height: '100%'}}
-        >
-            <View style={styles.inputContainer}> 
-              <Icon name="search" color="#BABABA" size={30}/>
-              <TextInput style={styles.searchInput} placeholder="CPF do Paciente"/>
-            </View>
+  return (
+    <ImageBackground
+      style={styles.container}
+      source={require('../../../assets/logo.jpg')}
+      imageStyle={{ width: '100%', height: '100%' }}
+    >
+      <View style={styles.inputContainer}>
+        <Icon name="search" color="#BABABA" size={30} />
+        <TextInput style={styles.searchInput} placeholder="CPF do Paciente" />
+      </View>
 
-            <Text style={styles.pacientName}> Paciente - Fulano </Text>
+      <Text style={styles.pacientName}> Paciente - Fulano </Text>
 
-            <View style={styles.buttonsContainer}>
-              <TouchableOpacity style={styles.button} onPress={()=>{}}>
-                <Text style={styles.buttonText}> Ficha Médica </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.button} onPress={()=>{}}>
-                <Text style={styles.buttonText}> Arquivos </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.button} onPress={()=>{}}>
-                <Text style={styles.buttonText}> Solicitar Acesso </Text>
-              </TouchableOpacity>
-            </View>
-
-        </ImageBackground>
-    )
+      <View style={styles.buttonsContainer}>
+        <Button title="Ficha Médica" onPress={() => {}} />
+        <Button title="Arquivos" onPress={() => {}} />
+        <Button title="Solicitar Acesso" onPress={() => {}} />
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default SearchPacient;
