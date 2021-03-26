6ea04e47cc79a1bd53913c9c9ce98d9cdddafdd9
Arthur <arthur001123@gmail.com>
2021-03-26 11:43:31 -0300

    [#48] Criando componente de botão de solicitação de acesso
    Co-Authored-by: Fellipe Araujo <araujo.fellipe@hotmail.com>

---
+++ frontend/src/components/FloatingAccessButton/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import styles from "./styles";
+
+const FloatingAccessButton = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
+      <Text style={styles.buttonText}> Solicitar Acesso Completo </Text>
+    </TouchableOpacity>
+  );
+};
+export default FloatingAccessButton;
---
+++ frontend/src/components/FloatingAccessButton/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    paddingVertical: "5%",
+    width: "90%",
+    borderRadius: 8,
+    backgroundColor: "#7E162B",
+    alignSelf: "center",
+    marginVertical: 10,
+    bottom: 10,
+    position: "absolute",
+    zIndex: 3,
+  },
+
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "#FFF",
+  },
+});
+
+export default styles;
