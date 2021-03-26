564ace7b23651abe6c7fbb0760e9f45ffba94602
Arthur <arthur001123@gmail.com>
2021-03-26 11:44:48 -0300

    Atualizando rotas e mocks
    Co-Authored-by: Fellipe Araujo <araujo.fellipe@hotmail.com>

--- frontend/src/utils/mocks.js
+++ frontend/src/utils/mocks.js
@@ -1,63 +1,130 @@
 export const user = {
-  name: 'Lorem Ipsum',
+  name: "Lorem Ipsum",
   age: 35,
   height: 170,
   weight: 70,
-  bloodGroup: 'AB+',
+  bloodGroup: "AB+",
   medicament: [
     {
       id: 1,
-      name: 'Rivotril  -  50mg',
+      name: "Rivotril  -  50mg",
     },
     {
       id: 2,
-      name: 'Roacutan - 100mg',
+      name: "Roacutan - 100mg",
     },
   ],
   allergy: [
     {
       id: 1,
-      name: 'Polén',
+      name: "Polén",
     },
     {
       id: 2,
-      name: 'Amendoim',
+      name: "Amendoim",
     },
   ],
   emergencyContacts: [
     {
       id: 1,
-      name: 'Lorem Ipsum Jr.',
-      number: '(61) 99999-9999  ',
+      name: "Lorem Ipsum Jr.",
+      number: "(61) 99999-9999  ",
     },
     {
       id: 2,
-      name: 'Lorem Ipsum Jr.',
-      number: '(61) 99999-9999  ',
+      name: "Lorem Ipsum Jr.",
+      number: "(61) 99999-9999  ",
     },
   ],
   folders: [
     {
       id: 1,
-      name: 'Exame de Sangue',
+      name: "Exame de Sangue",
       files: [
         {
           id: 1,
-          name: 'Exame do dia 20/11/2020',
+          name: "Exame do dia 20/11/2020",
         },
         {
           id: 2,
-          name: 'Exame do dia 15/01/2021',
+          name: "Exame do dia 15/01/2021",
         },
       ],
     },
     {
       id: 2,
-      name: 'Receitas',
+      name: "Receitas",
       files: [
         {
           id: 1,
-          name: 'Receita do Rivotril',
+          name: "Receita do Rivotril",
+        },
+      ],
+    },
+  ],
+};
+
+export const pacient = {
+  name: "Lorem Ipsum",
+  age: 35,
+  height: 170,
+  weight: 70,
+  bloodGroup: "AB+",
+  medicament: [
+    {
+      id: 1,
+      name: "Rivotril  -  50mg",
+    },
+    {
+      id: 2,
+      name: "Roacutan - 100mg",
+    },
+  ],
+  allergy: [
+    {
+      id: 1,
+      name: "Polén",
+    },
+    {
+      id: 2,
+      name: "Amendoim",
+    },
+  ],
+  emergencyContacts: [
+    {
+      id: 1,
+      name: "Lorem Ipsum Jr.",
+      number: "(61) 99999-9999  ",
+    },
+    {
+      id: 2,
+      name: "Lorem Ipsum Jr.",
+      number: "(61) 99999-9999  ",
+    },
+  ],
+  hasFullAccess: false,
+  folders: [
+    {
+      id: 1,
+      name: "Exame de Sangue",
+      files: [
+        {
+          id: 1,
+          name: "Exame do dia 20/11/2020",
+        },
+        {
+          id: 2,
+          name: "Exame do dia 15/01/2021",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Receitas",
+      files: [
+        {
+          id: 1,
+          name: "Receita do Rivotril",
         },
       ],
     },
