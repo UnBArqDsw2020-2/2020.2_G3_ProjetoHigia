fbea5bcb27de3546f6ddc4655e6f5ddb872a6df3
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-04-05 19:10:21 -0300

    [#91] Corrigindo estilização do dropdown.
    Co-Authored-by: Gabriel Hussein <gabrielhussein83@gmail.com>
    Co-Authored-by: Aline <aline.helena.lermen@gmail.com>

--- frontend/src/components/DropdownButton/styles.js
+++ frontend/src/components/DropdownButton/styles.js
@@ -1,15 +1,26 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  dropdownContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    // justifyContent: 'space-between',
+    marginBottom: 15,
+    borderBottomWidth: 1.0,
+    borderColor: '#7E162B',
+  },
+  // iconContainer: {
+  //   marginLeft: 10
+  // },
   dropdownButtonContainer: {
+    width: '70%',
     textAlign: 'left',
     fontWeight: 'bold',
     fontSize: 15,
     backgroundColor: '#FFF',
-    borderColor: '#7E162B',
     padding: 7,
-    marginBottom: 15,
-    borderBottomWidth: 1.0,
   },
 
   dropdownButtonText: {
