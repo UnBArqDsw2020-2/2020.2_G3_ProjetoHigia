13aaee8c1a455833da657b06500d9cfddcbb0040
Arthur <arthur001123@gmail.com>
2021-05-01 10:30:55 -0300

    [118] criando alerta de solicitação de acesso ao receber mensagem via socket

--- frontend/App.js
+++ frontend/App.js
@@ -6,20 +6,44 @@ import apiKeys from "./src/utils/firebaseKeys.js";
 import { useFonts, Junge_400Regular } from "@expo-google-fonts/junge";
 import AppLoading from "expo-app-loading";
 import Routes from "./src/routes";
-import { StatusBar } from "react-native";
+import { Alert, StatusBar } from "react-native";
 import { Provider } from "react-redux";
 import LoadingCircle from "./src/components/Loading";
 import loadingStore from "./src/store/loading";
 import WebSocket from "./src/services/websocket.js";
+import { useAuth } from "./src/context/auth";
 
 const App = () => {
 	var [fontsLoaded] = useFonts({ Junge_400Regular });
+	const authContext = useAuth();
 
 	if (!fontsLoaded) {
 		return <AppLoading />;
 	}
 
 	WebSocket.connect();
+
+	let doctor;
+	WebSocket.socket.on("accessRequested", (crm) => {
+		api.get(`user?crm=${crm}`).then((response) => {
+			doctor = response.data;
+			Alert.alert(
+				"Nova solicitação de acesso",
+				"Deseja conceder acesso ao Médico " + doctor.name + "?",
+				[
+					{
+						text: "Sim",
+						onPress: () =>
+							permitirAcesso(authContext.currentUser, doctor),
+					},
+					{
+						text: "Cancelar",
+						style: "cancel",
+					},
+				]
+			);
+		});
+	});
 
 	if (!firebase.apps.length) {
 		console.log("Connected with Firebase");
@@ -43,3 +67,16 @@ const App = () => {
 };
 
 export default App;
+
+function permitirAcesso(user, doctor) {
+	api.post("/allowAccess", {
+		id: user._id,
+		crm: doctor.crm,
+	})
+		.then(() => {
+			alert("Permissão dada com sucesso");
+		})
+		.catch(() => {
+			alert("Erro ao enviar solicitação");
+		});
+}
